d040332afb4055a5d010d3f3330ab625ab3bd375
Lucas Livchits <livchitsl@gmail.com>
2021-04-23 20:23:36 -0300

    Add options to order and filter bands

--- src/components/ShowBandsOptions.jsx
+++ src/components/ShowBandsOptions.jsx
@@ -1,7 +1,32 @@
 import * as React from 'react';
 
+import useGetData from '../hooks/useGetData';
+
 function ShowBandsOptions() {
-  return <div>opciones</div>;
+  const { VITE_GENRES: genresUrl } = import.meta.env;
+
+  const { data: genres, error } = useGetData(genresUrl);
+
+  if (error) return <div>Something went wrong</div>;
+
+  return (
+    <form>
+      <div>
+        <input checked id='asc' name='order' type='radio' value='asc' />
+        <label htmlFor='asc'>A-Z</label>
+        <input id='desc' name='order' type='radio' value='desc' />
+        <label htmlFor='desc'>Z-A</label>
+      </div>
+      <select id='genres' name='genres'>
+        <option value=''>Filter for a genre</option>
+        {genres?.map(({ name, code }) => (
+          <option key={code} value={code}>
+            {name}
+          </option>
+        ))}
+      </select>
+    </form>
+  );
 }
 
 export default ShowBandsOptions;
