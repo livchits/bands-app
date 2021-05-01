ec7026845669059e3e754306584477d24be8c92c
Lucas Livchits <livchitsl@gmail.com>
2021-05-01 11:23:15 -0300

    Change Container tag

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -3,9 +3,9 @@ import PropTypes from 'prop-types';
 
 function Container({ children }) {
   return (
-    <main className='flex flex-col justify-center w-11/12 max-w-2xl mx-auto bg-red-600 rounded-md shadow-md h-5/6 font-chivo text-gray-50 bg-opacity-90'>
+    <div className='flex flex-col justify-center w-11/12 max-w-2xl mx-auto bg-red-600 rounded-md shadow-md h-5/6 font-chivo text-gray-50 bg-opacity-90'>
       {children}
-    </main>
+    </div>
   );
 }
 
