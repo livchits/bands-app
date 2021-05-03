7461b7fe78846209da08009f490dae267ffcef57
Lucas Livchits <livchitsl@gmail.com>
2021-05-02 23:11:18 -0300

    Remover flexbox

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
-
 function Container({ children }) {
   return (
-    <div className='flex flex-col justify-center w-11/12 max-w-2xl mx-auto bg-red-600 rounded-md shadow-md h-5/6 font-chivo text-gray-50 bg-opacity-90'>
+    <div className='w-11/12 max-w-2xl mx-auto bg-red-600 rounded-md shadow-md h-5/6 font-chivo text-gray-50 bg-opacity-90'>
       {children}
     </div>
   );
