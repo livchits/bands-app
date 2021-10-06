eb63bd1905d93d25b0de75e9a5aed8dc8e86c650
Lucas Livchits <livchitsl@gmail.com>
2021-10-05 23:35:48 -0300

    Add types for filterByGenre util

--- src/utils/filterByGenre.ts
+++ src/utils/filterByGenre.ts
@@ -1,4 +1,6 @@
-function filterByGenre(bands, filterCriteria) {
+import { BandData, Genre } from './../types';
+
+function filterByGenre(bands: BandData[], filterCriteria: Genre) {
   return filterCriteria === 'all'
     ? bands
     : bands.filter(({ genreCode }) => genreCode === filterCriteria);
