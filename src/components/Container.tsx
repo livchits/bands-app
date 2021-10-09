7b0789bdef97dd76e26fdf879f72bae9010084a9
Lucas Livchits <livchitsl@gmail.com>
2021-10-09 12:48:36 -0300

    Add type for Container component

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
-function Container({ children }) {
+
+function Container({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex flex-col w-11/12 h-screen max-w-2xl mx-auto my-4 overflow-hidden bg-red-600 rounded-md shadow-md font-chivo text-gray-50 bg-opacity-80'>
       {children}
     </div>
   );
 }
-
-Container.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.node),
-    PropTypes.node,
-  ]),
-};
 
 export default Container;
