551aa1b5b7bc56607a960e79faaa675d8d51f16d
Lucas Livchits <livchitsl@gmail.com>
2021-10-10 14:03:37 -0300

    Add types form Albums component

--- src/components/Albums.tsx
+++ src/components/Albums.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
-function Albums({ albumsData }) {
+import { AlbumData } from '../types';
+
+function Albums({ albumsData }: { albumsData: AlbumData[] }) {
   return (
     <section className='mt-2 text-xl'>
       <h2 className='py-1 font-black'>Albums:</h2>
@@ -15,16 +16,5 @@ function Albums({ albumsData }) {
     </section>
   );
 }
-
-Albums.propTypes = {
-  albumsData: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number,
-      bandId: PropTypes.number,
-      name: PropTypes.string,
-      year: PropTypes.number,
-    })
-  ),
-};
 
 export default Albums;
